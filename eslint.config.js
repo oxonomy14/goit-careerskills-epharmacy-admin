9f85a7df555ae6c57a53d38a85ebbc4644f2cd8a
oxonomy14 <oxonomy14@gmail.com>
2026-03-13 16:40:12 +0700

    chore: initialize ePharmacy admin dashboard with Vite and React

---
+++ eslint.config.js
@@ -0,0 +1,23 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import prettier from 'eslint-plugin-prettier';
+import configPrettier from 'eslint-config-prettier';
+
+export default [
+  pluginJs.configs.recommended,
+  configPrettier,
+  {
+    files: ['src/**/*.js'],
+    languageOptions: { globals: globals.node },
+    plugins: {
+      prettier,
+    },
+    rules: {
+      semi: 'error',
+      'no-unused-vars': ['error', { args: 'none' }],
+      'no-undef': 'error',
+      'prettier/prettier': 'error',
+      'react/prop-types': 0,
+    },
+  },
+];
